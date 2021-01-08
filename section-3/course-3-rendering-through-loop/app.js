aee916220f5949701ee88e6eb371d0e2e9437d01
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-01-08 10:57:25 +0700

    Rendering through loops

--- section-3/course-3-rendering-through-loop/app.js
+++ section-3/course-3-rendering-through-loop/app.js
@@ -2,13 +2,20 @@ const app = {
   data() {
     return {
       goal: null,
-      goals: []
+      goals: [],
+      student: {
+        name: 'Joko',
+        gender: 'Male'
+      }
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.goal)
       this.goal = null
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1)
     }
   }
 };
