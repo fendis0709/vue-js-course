53bb30ed6ad55bf36bf08b991d0cc6b17a7b6485
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-02-05 11:52:49 +0700

    Assignment vue app

---
+++ section-8/assignment-1-props-and-emits-event/src/components/ActiveUser.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <user-data @add-user="addNewUser"></user-data>
+    <hr />
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <strong>{{ user.name }}</strong> - {{ user.age }} year(s) old
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Joko",
+          age: 23,
+        },
+      ],
+    };
+  },
+  methods: {
+    addNewUser(newUser) {
+      const counter = this.users.length;
+      const user = {
+        id: counter + 1,
+        name: newUser.name,
+        age: newUser.age,
+      };
+      this.users.push(user);
+    },
+  },
+};
+</script>
---
+++ section-8/assignment-1-props-and-emits-event/src/components/UserData.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form action="#" @submit.prevent="submitData">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" v-model="user.name" autofocus required />
+      </div>
+      <div>
+        <label for="age">Age</label>
+        <input type="number" min="0" max="100" v-model="user.age" />
+      </div>
+      <div class="submit-button">
+        <button>Add User</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: null,
+        age: null,
+      },
+    };
+  },
+  emits: ["add-user"],
+  methods: {
+    submitData() {
+      this.$emit("add-user", this.user);
+      this.user.name = null;
+      this.user.age = 0;
+    },
+  },
+};
+</script>
