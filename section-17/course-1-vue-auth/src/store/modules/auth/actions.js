4590a5bc66edfca0faea928477d1380c27d49e61
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-02-11 17:39:44 +0700

    Auto login handler

--- section-17/course-1-vue-auth/src/store/modules/auth/actions.js
+++ section-17/course-1-vue-auth/src/store/modules/auth/actions.js
@@ -2,42 +2,26 @@ const apiKey = 'AIzaSyChJHRw8HNpOrHK3ahvOH6DQhaoNgC-48M';
 
 export default {
   async signin(context, payload) {
-    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true
-      })
-    })
-
-    const responseJson = await response.json()
-
-    if (!response.ok) {
-      let errorMessage = 'Failed to login user.';
-      switch (responseJson.error.message) {
-        case 'EMAIL_NOT_FOUND':
-          errorMessage = 'Email not registered.';
-          break;
-        case 'INVALID_PASSWORD':
-          errorMessage = 'Invalid password.';
-          break;
-        case 'USER_DISABLED':
-          errorMessage = 'User disabled.';
-          break;
-      }
-
-      throw new Error(errorMessage);
-    }
-
-    context.commit('setUser', {
-      userId: responseJson.localId,
-      token: responseJson.idToken,
-      tokenLifetime: responseJson.expiresIn
+    await context.dispatch('auth', {
+      ...payload,
+      mode: 'signin'
     })
   },
   async signup(context, payload) {
-    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
+    await context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    })
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+    if (mode === 'signin') {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
+    }
+
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
@@ -49,9 +33,28 @@ export default {
     const responseJson = await response.json()
 
     if (!response.ok) {
-      let errorMessage = 'Failed to register user.';
-      if (responseJson.error.message == 'EMAIL_EXISTS') {
-        errorMessage = 'Email already registered.';
+      let errorMessage = 'Something went wrong, please try again later.';
+
+      if (mode === 'signin') {
+        errorMessage = 'Failed to login user.';
+        switch (responseJson.error.message) {
+          case 'EMAIL_NOT_FOUND':
+            errorMessage = 'Email not registered.';
+            break;
+          case 'INVALID_PASSWORD':
+            errorMessage = 'Invalid password.';
+            break;
+          case 'USER_DISABLED':
+            errorMessage = 'User disabled.';
+            break;
+        }
+      }
+
+      if (mode === 'signup') {
+        errorMessage = 'Failed to register user.';
+        if (responseJson.error.message == 'EMAIL_EXISTS') {
+          errorMessage = 'Email already registered.';
+        }
       }
 
       throw new Error(errorMessage);
@@ -62,6 +65,23 @@ export default {
       token: responseJson.idToken,
       tokenLifetime: responseJson.expiresIn
     })
+
+    context.commit('storeUser', {
+      userId: responseJson.localId,
+      token: responseJson.idToken
+    })
+  },
+  autoSignin(context) {
+    const vueToken = localStorage.getItem('vue__token');
+    const vueUserId = localStorage.getItem('vue__user_id');
+
+    if (vueToken && vueUserId) {
+      context.commit('setUser', {
+        userId: vueUserId,
+        token: vueToken,
+        tokenLifetime: null
+      })
+    }
   },
   signout(context) {
     context.commit('setUser', {
--- section-17/course-1-vue-auth/src/store/modules/auth/mutations.js
+++ section-17/course-1-vue-auth/src/store/modules/auth/mutations.js
@@ -3,5 +3,9 @@ export default {
     state.userId = data.userId;
     state.token = data.token;
     state.tokenLifetime = data.tokenLifetime;
+  },
+  storeUser(_, data) {
+    localStorage.setItem('vue__token', data.token)
+    localStorage.setItem('vue__user_id', data.userId)
   }
 }
