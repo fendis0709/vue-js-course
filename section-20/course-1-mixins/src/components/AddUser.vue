8eca1b7bfd505f64c5912abc953d80332225def3
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-02-15 15:12:15 +0700

    Mixin concept

---
+++ section-20/course-1-mixins/src/components/AddUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+      <p>Do you want to continue with adding a user?</p>
+    </user-alert>
+    <section>
+      <h2>Add a User</h2>
+      <button @click="showAlert">Add User</button>
+    </section>
+  </div>
+</template>
+
+<script>
+import AlertMixin from '../mixins/alert.js';
+
+export default {
+  mixins: [AlertMixin],
+};
+</script>
