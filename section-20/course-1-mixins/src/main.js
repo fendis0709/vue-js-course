c3efa4895b02752d4a322f444721edee4cd2e4cf
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-02-15 16:37:47 +0700

    Global mixin

--- section-20/course-1-mixins/src/main.js
+++ section-20/course-1-mixins/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import LoggerMixin from './mixins/logger.js';
 
-const app = createApp(App)
-
-app.mount('#app');
+createApp(App)
+  .mixin(LoggerMixin)
+  .mount('#app');
---
+++ section-20/course-1-mixins/src/mixins/logger.js
@@ -0,0 +1,6 @@
+export default {
+  mounted() {
+    const date = new Date();
+    console.log('Hello, world! ' + date);
+  }
+}
