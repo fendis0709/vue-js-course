09776e6310b79166df91591b93f9c6dac58023e1
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2021-02-23 17:35:28 +0700

    Add basic routing

--- section-13/course-1-vue-router/src/main.js
+++ section-13/course-1-vue-router/src/main.js
@@ -1,7 +1,26 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import Teams from './components/teams/TeamsList.vue';
+import Users from './components/users/UsersList.vue';
 
 const app = createApp(App)
 
-app.mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: Teams
+    },
+    {
+      path: '/users',
+      component: Users
+    }
+  ]
+})
+
+app
+  .use(router)
+  .mount('#app');
